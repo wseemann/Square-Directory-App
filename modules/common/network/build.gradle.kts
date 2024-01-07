906a0a97776ee207f0f374e2d7be280d73630471
wseemann <wseemann@gmail.com>
2024-01-07 00:08:03 -0800

    Remove unused packages

--- modules/common/network/build.gradle.kts
+++ modules/common/network/build.gradle.kts
@@ -42,11 +42,4 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.47")
     kapt("com.google.dagger:hilt-android-compiler:2.47")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
