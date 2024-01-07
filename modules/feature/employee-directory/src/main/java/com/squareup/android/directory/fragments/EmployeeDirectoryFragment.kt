6d0cb51cf341da5e8202b3e4ad4798a7c6f1e8ef
wseemann <wseemann@gmail.com>
2024-01-07 00:45:58 -0800

    Display a toast when an error occurs

--- modules/feature/employee-directory/src/main/java/com/squareup/android/directory/fragments/EmployeeDirectoryFragment.kt
+++ modules/feature/employee-directory/src/main/java/com/squareup/android/directory/fragments/EmployeeDirectoryFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.squareup.android.directory.R
 import com.squareup.android.directory.adapters.EmployeeAdapter
 import com.squareup.android.directory.databinding.FragmentEmployeeDirectoryBinding
 import com.squareup.android.directory.model.Employee
@@ -48,7 +50,7 @@ class EmployeeDirectoryFragment : Fragment(), SwipeRefreshLayout.OnRefreshListen
 
         initFlows()
         binding.progressIndicator.visibility = View.VISIBLE
-        loadData(true)
+        loadData(fromCache = true)
     }
 
     override fun onDestroyView() {
@@ -62,19 +64,17 @@ class EmployeeDirectoryFragment : Fragment(), SwipeRefreshLayout.OnRefreshListen
                 when (state) {
                     is UiState.Loading -> Unit
                     is UiState.Error -> {
-                        binding.swipeRefreshLayout.isRefreshing = false
-                        binding.progressIndicator.visibility = View.GONE
+                        showEmptyView()
+                        hideLoadingViews()
+                        showErrorToast()
                     }
                     is UiState.Success -> {
                         if (state.data.isEmpty()) {
-                            binding.emptyListTextview.visibility = View.VISIBLE
-                            binding.employeeRecyclerView.visibility = View.GONE
+                            showEmptyView()
                         } else {
-                            binding.employeeRecyclerView.visibility = View.VISIBLE
-                            employeeAdapter.updateData(state.data)
+                            updateEmployeeAdapter(state.data)
                         }
-                        binding.swipeRefreshLayout.isRefreshing = false
-                        binding.progressIndicator.visibility = View.GONE
+                        hideLoadingViews()
                     }
                 }
             }
@@ -83,10 +83,29 @@ class EmployeeDirectoryFragment : Fragment(), SwipeRefreshLayout.OnRefreshListen
 
     override fun onRefresh() {
         binding.swipeRefreshLayout.isRefreshing = true
-        loadData(false)
+        loadData( fromCache = false)
     }
 
     private fun loadData(fromCache: Boolean) {
         employeeDirectoryViewModel.getEmployeeData(fromCache)
+    }
+
+    private fun updateEmployeeAdapter(employees: List<Employee>) {
+        binding.employeeRecyclerView.visibility = View.VISIBLE
+        employeeAdapter.updateData(employees)
+    }
+
+    private fun showEmptyView() {
+        binding.emptyListTextview.visibility = View.VISIBLE
+        binding.employeeRecyclerView.visibility = View.GONE
+    }
+
+    private fun hideLoadingViews() {
+        binding.swipeRefreshLayout.isRefreshing = false
+        binding.progressIndicator.visibility = View.GONE
+    }
+
+    private fun showErrorToast() {
+        Toast.makeText(requireContext(), getString(R.string.api_error_message), Toast.LENGTH_SHORT).show()
     }
 }
