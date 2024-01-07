4cfc849d616259ec17c6d3d41c7f03b8ccc51b93
wseemann <wseemann@gmail.com>
2024-01-07 00:44:54 -0800

    Remove unused code

--- modules/feature/employee-directory/src/main/java/com/squareup/android/directory/model/UIState.kt
+++ modules/feature/employee-directory/src/main/java/com/squareup/android/directory/model/UIState.kt
@@ -8,9 +8,3 @@ sealed interface UiState<out R> {
     data class Error(val errorMessage: String? = null) : UiState<Nothing>
     data class Success<T>(val data: T) : UiState<T>
 }
-
-val <T> UiState<T>.data: T?
-    get() = (this as? UiState.Success)?.data
-
-val <T> UiState<T>.errorMessage: String?
-    get() = (this as? UiState.Error)?.errorMessage
